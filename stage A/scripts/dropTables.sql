0a50d0c5f5719ddb9489d3982fa7c13d958d9907
Levi Yitzhak grinfeld <grindeld@g.jct.ac.il>
2025-04-01 16:58:17 +0300

    Update dropTables.sql

--- stage A/scripts/dropTables.sql
+++ stage A/scripts/dropTables.sql
@@ -1,1 +1,6 @@
-
+DROP TABLE IF EXISTS public.appointments CASCADE;
+DROP TABLE IF EXISTS public.residentmedications_ CASCADE;
+DROP TABLE IF EXISTS public.medicaltreatments CASCADE;
+DROP TABLE IF EXISTS public.medications CASCADE;
+DROP TABLE IF EXISTS public.residents CASCADE;
+DROP TABLE IF EXISTS public.doctors CASCADE;
