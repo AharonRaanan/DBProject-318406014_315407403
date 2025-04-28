452b684da4115162774951d45285ca4dd64f4fd5
aharo <araanan@g.jct.ac.il>
2025-04-28 15:05:14 +0300

    Added untracked files

---
+++ stage A/stage A/scripts/selectAll.sql
@@ -0,0 +1,17 @@
+-- To select all records from the Doctors table
+SELECT * FROM doctors;
+
+-- To select all records from the Residents table
+SELECT * FROM residents;
+
+-- To select all records from the Medications table
+SELECT * FROM medications;
+
+-- To select all records from the ResidentMedications table
+SELECT * FROM residentmedications_;
+
+-- To select all records from the MedicalTreatments table
+SELECT * FROM medicaltreatments;
+
+-- To select all records from the Appointments table
+SELECT * FROM appointments;
